c771ed6babb1e731b058562e9d9aff03d7a40781
Daniel Bennett and Katrina Hockman <katrina@>
2018-10-30 15:19:05 -0700

    Fix object & prototype, Add Alex to group

--- js/scripts.js
+++ js/scripts.js
@@ -1,67 +1,36 @@
-//business logic
-function FinalTicket() {
 
-  this.tickets = [],
-  this.currentId = 0
-}
-
-FinalTicket.prototype.addTicket = function(ticket) {
-  ticket.id = this.assignId();
-  this.tickets.push(ticket);
-}
-
-FinalTicket.prototype.assignId = function () {
-  this.currentId +=1;
-  return this.currentId;
-}
-
-FinalTicket.prototype.findPrice = function(price) {
-  debugger
-  for (var i=0; i<this.tickets.length; i++) {
-    if (this.tickets[0] <= 12 || this.tickets[0] >= 50) {
-      $("p#result").text("$5");
-    } else if (this.tickets[1] <5) {
-        $("p#result").text("$5");
-      } else {
-        $("p#result").text("$10");
-     }
-    }
-  }
 
 function Ticket(personAge, movieTime) {
-  debugger
   this.personAge = personAge,
-  this.movieTime = movieTime
-  // this.id;
+  this.movieTime = movieTime,
+  this.price = 10
 }
 
-var finalTicket = new FinalTicket();
-// var discountTicket = ("$5");
-// var spendyTicket = ("$10");
+Ticket.prototype.findPrice = function() {
+  //debugger;
+    if ( this.personAge <= 12) {
+      this.price -= 5;
+      console.log("A discount! " + this.price);
+    };
+};
 
-// function displayMovieDetails(showTimesToDisplay) {
-//   var movieList = $("ul#movies");
-//   var htmlForMovieInfo = "";
-//   showTimesToDisplay.movies.forEach(function(movie) {
-//     htmlForMovieInfo += "<li id=" + ticket.id + ">" + "$10" + "</li>";
-//   });
-//   contactsList.html(htmlForContactInfo)
-// };
-
+//UI Logic
 $(document).ready(function(){
   //attachContactListeners();
   $("#movie").submit(function(event){
-debugger
     event.preventDefault();
+    //debugger;
+    //getting info from user
     var inputtedMovieTime = parseInt($("input#movie-time").val());
     var inputtedPersonAge = parseInt($("input#age").val());
-    $("input#movie-time").val("");
-    $("input#age").val("");
-
+    //birthing a new object
     var newTicket = new Ticket(inputtedPersonAge, inputtedMovieTime);
-    finalTicket.addTicket(newTicket);
-    finalTicket.findPrice(newTicket);
-    console.log(newTicket)
+
+    //calling the prototype on the object
+    newTicket.findPrice();
+
+    // finalTicket.addTicket(newTicket);
+    console.log("The user will see: " + newTicket.price)
     //displayContactDetails(addressBook);
   })
 })

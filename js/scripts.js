db9c99da937c272c68f45b2bc0a6cbc36a2654eb
Daniel Bennett and Katrina Hockman <daniel@>
2018-10-30 15:40:55 -0700

    add display for user interface, tidy up code.

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,24 @@
-
-
 function Ticket(personAge, movieTime) {
   this.personAge = personAge,
   this.movieTime = movieTime,
   this.price = 10
 }
-
 Ticket.prototype.findPrice = function() {
-  //debugger;
-    if ( this.personAge <= 12) {
+    if (( this.personAge <= 12) || (this.personAge >=50)) {
       this.price -= 5;
-      console.log("A discount! " + this.price);
-    };
+    } else if (this.movieTime <5){
+      this.price -=5;
+      result.push(findPrice())
+    }
 };
-
 //UI Logic
 $(document).ready(function(){
-  //attachContactListeners();
   $("#movie").submit(function(event){
     event.preventDefault();
-    //debugger;
-    //getting info from user
     var inputtedMovieTime = parseInt($("input#movie-time").val());
     var inputtedPersonAge = parseInt($("input#age").val());
-    //birthing a new object
     var newTicket = new Ticket(inputtedPersonAge, inputtedMovieTime);
-
-    //calling the prototype on the object
     newTicket.findPrice();
-
-    // finalTicket.addTicket(newTicket);
-    console.log("The user will see: " + newTicket.price)
-    //displayContactDetails(addressBook);
+    $("#result").text("$" + newTicket.price);
   })
 })
